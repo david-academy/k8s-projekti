65fde5a22b63a31c00017ef9be600887fc2a6669
david-academy <david.andberg@gmail.com>
2020-05-04 09:46:47 +0300

    docker-compose and dockerfiles updated

---
+++ client/build/precache-manifest.e05688623cadef505bfb52a77264faf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb424a47b28002ffac5901b06a4de71d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c550b4881ada187b6115",
+    "url": "/static/js/2.51f5612b.chunk.js"
+  },
+  {
+    "revision": "73f631d49e0419a420c7f58042b4e6ae",
+    "url": "/static/js/2.51f5612b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47b41ddc18a245effb7",
+    "url": "/static/js/main.4bd52256.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
